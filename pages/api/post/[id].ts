3e52a17d0895b35210d06be345d430d27881b87b
ihssandk <dooaknshop@gmail.com>
2022-10-14 21:28:52 +0100

    fix api

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -5,7 +5,7 @@ import { client } from '../../../utils/client';
 import { postDetailQuery } from '../../../utils/queries';
 
 
-export default async function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
+export default async function handler( req: NextApiRequest, res: NextApiResponse) {
   if(req.method === 'GET'){
     const {id} = req.query;
     const query = postDetailQuery(id);
--- pages/api/search/[searchTerm].ts
+++ pages/api/search/[searchTerm].ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import {client} from '../../../utils/client';
 import {searchPostsQuery} from "../../../utils/queries";
 
-export default async function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
+export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
   if(req.method === 'GET'){
     const {searchTerm} = req.query;
 
